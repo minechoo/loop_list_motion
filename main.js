02b9782c367f4e9eac3f451cca86acb44bc46c47
minechoo <choominhae@gmail.com>
2023-06-14 10:40:01 +0900

    완성

--- main.js
+++ main.js
@@ -5,8 +5,16 @@ const next = document.querySelector('.next');
 
 next.addEventListener('click', () => {
 	frame.append(frame.firstElementChild);
+	next.classList.add('on');
+	setTimeout(() => {
+		next.classList.remove('on');
+	}, 500);
 });
 
 prev.addEventListener('click', () => {
 	frame.prepend(frame.lastElementChild);
+	prev.classList.add('on');
+	setTimeout(() => {
+		prev.classList.remove('on');
+	}, 500);
 });
